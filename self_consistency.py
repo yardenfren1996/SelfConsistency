d7fd109da43d5481592ccf193e1e426c6c8e066e
MarkNoaTAU <noamark@mail.tau.ac.il>
2023-09-23 18:44:11 +0300

    Add setup file, change model size of t5 in self-consistency.

--- self_consistency.py
+++ self_consistency.py
@@ -10,7 +10,8 @@ from llms import Pegasus, Llama_2, Flan_T5
 
 CONFIG = {'pegasus': {'cls': Pegasus, 'config': {'model_name': "google/pegasus-xsum"}},
           'llama_2': {'cls': Llama_2, 'config': {'model_name': "meta-llama/Llama-2-7b-chat-hf"}},
-          'flan_t5': {'cls': Flan_T5, 'config': {'model_name': "google/flan-t5-xl"}}}
+          'flan_t5_xl': {'cls': Flan_T5, 'config': {'model_name': "google/flan-t5-xl"}},
+          'flan_t5': {'cls': Flan_T5, 'config': {'model_name': "google/flan-t5-large"}}}
 
 
 def parse_args():
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='SelfConsistency',
+    packages=find_packages(include=['SelfConsistency']),
+    version='0.1.0',
+    description='Research project, examine the impact of pyparsing and different method for self-consistency on '
+                'hallucination and summary quality in LLM. Final project for NLP course in TLV university 2023.',
+    author=' Noa Mark & Yarden Frenkel',
+    license='Public',
+)
