9bc323ae7028f4186cf6502338b1235faff78dd6
MarkNoaTAU <noamark@mail.tau.ac.il>
2023-09-24 15:21:36 +0300

    Add partition type and mitigation method to run (parapharsing/ self consistency) to args.

--- slurm_excecuter.py
+++ slurm_excecuter.py
@@ -16,9 +16,16 @@ def parse_args():
                         help="Desired name for the output inference file.")
     parser.add_argument("--no_generations", type=int, default=10, help="number of generations per sentence.")
     parser.add_argument("--no_seeds", type=int, default=5, help="number of seeds per document.")
+    parser.add_argument("--method_type", type=str, default='self_consistency',
+                        help="can be one of self_consistency or paraphrasing")
+    parser.add_argument("--partition", type=str, default='studentkillable',
+                        help="can be one of studentkillable or studentbatch")
+
     args = parser.parse_args()
     return args
 
+
+# TODO: Add whether I run self_consistency or paraphrasing !! in the args !
 
 def main():
     try:
@@ -30,16 +37,20 @@ def main():
         for batch_num, (start_idx, end_idx) in enumerate(chunk_ranges):
             # if batch_num not in [2, 4, 6]:
             #     continue
+            if args.method_type == 'self_consistency':
+                ex_line = 'python self_consistency.py --model {args.model} --dataset {args.dataset} --from_index {start_idx} --to_index {end_idx} --results_filename "distributed/{args.results_filename}_{batch_num}" --no_generations {args.no_generations} --no_seeds {args.no_seeds}'
+            elif args.method_type == 'paraphrasing':
+                ex_line = 'python paraphrasing.py --model {args.model} --dataset {args.dataset} --from_index {start_idx} --to_index {end_idx} --results_filename "distributed/{args.results_filename}_{batch_num}" --no_generations {args.no_generations}'
+            else:
+                raise ValueError("Method type is not supported, can be one of the following: self consistency / paraphrasing.")
             sbatch_script = f"""#! /bin/sh
 #SBATCH --job-name={args.job_name}_{batch_num}
 #SBATCH --output=logs/{args.job_name}_{batch_num}.out
 #SBATCH --error=logs/{args.job_name}_{batch_num}.err
-#SBATCH --partition=studentkillable
-        
-python paraphrasing.py --model {args.model} --dataset {args.dataset} --from_index {start_idx} --to_index {end_idx} --results_filename "distributed/{args.results_filename}_{batch_num}" --no_generations {args.no_generations}
+#SBATCH --partition={args.partition}
+
+{ex_line}        
 """
-# python self_consistency.py --model {args.model} --dataset {args.dataset} --from_index {start_idx} --to_index {end_idx} --results_filename "distributed/{args.results_filename}_{batch_num}" --no_generations {args.no_generations} --no_seeds {args.no_seeds}
-#python paraphrasing.py --model {args.model} --dataset {args.dataset} --from_index {start_idx} --to_index {end_idx} --results_filename "distributed/{args.results_filename}_{batch_num}" --no_generations {args.no_generations}
             with open('evaluate.slurm', 'w') as f:
                 f.write(sbatch_script)
 
