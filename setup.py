c51430d6efd8b18ecbb1bb5a0e8271c35af3ac5a
MarkNoaTAU <noamark@mail.tau.ac.il>
2023-09-26 14:22:09 +0300

    reorganize files to package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='SelfConsistency',
-    packages=find_packages(include=['SelfConsistency']),
+    packages=['SelfConsistency'],
     version='0.1.0',
     description='Research project, examine the impact of pyparsing and different method for self-consistency on '
                 'hallucination and summary quality in LLM. Final project for NLP course in TLV university 2023.',
