8d083c7ae65d6452d02d6edbfdf4f26d94321729
MarkNoaTAU <noamark@mail.tau.ac.il>
2023-09-26 11:25:27 +0300

    fix

--- results/merge_results.py
+++ results/merge_results.py
@@ -6,17 +6,19 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--file_prefix", type=str, help="File prefix to merge.")
     parser.add_argument("--number_of_files", type=int, default=0, help="Number of files to merge.")
+    parser.add_argument("--output_path", type=str, default='',
+                        help="Path different from the default location for the output file.")
     args = parser.parse_args()
     return args
 
 
-def merge_pickels(file_prefix, number_of_files):
+def merge_pickels(file_prefix, number_of_files, output_path):
     try:
         res = {}
         for i in range(number_of_files):
             with open(f'distributed/{file_prefix}_{i}.pkl', 'rb') as f:
                 res.update(pickle.load(f))
-        with open(f'{file_prefix}.pkl', 'wb') as f:
+        with open(f'{output_path}{file_prefix}.pkl', 'wb') as f:
             pickle.dump(res, f)
         print(f'successfully merged {number_of_files} files into {file_prefix}.pkl')
     except Exception as e:
@@ -25,4 +27,4 @@ def merge_pickels(file_prefix, number_of_files):
 
 if __name__ == '__main__':
     args = parse_args()
-    merge_pickels(args.file_prefix, args.number_of_files)
+    merge_pickels(args.file_prefix, args.number_of_files, args.output_path)
